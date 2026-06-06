c891701be7a3acc67a8d7aa1e203e07174633246
Yan Bisiukou <y.bisiukou@evernode.tech>
2026-06-06 17:56:43 +0300

    add tasks 4 and 5 for lesson 4

---
+++ Lesson04_Arrays/L04.Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace L04.Task04;
+
+/*
+Создайте двумерный массив целых чисел. Выведите на консоль сумму всех
+элементов массива.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] twoDArray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        Console.Write("2d array: ");
+        
+        int sum = 0;
+        foreach (int num in twoDArray)
+        {
+            Console.Write(num + " ");
+            sum += num;
+        }
+        
+        Console.WriteLine("\nsum of elements: " + sum);
+    }
+}
---
+++ Lesson04_Arrays/L04.Task05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace L04.Task05;
+
+/*
+Создайте двумерный массив.
+Выведите на консоль диагонали массива.
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] twoDArray =
+        {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 10, 11, 12 },
+            {13, 14, 15, 16}
+        };
+
+        int size = twoDArray.GetLength(0);
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(twoDArray[i, i] + " ");
+        }
+        
+        Console.WriteLine();
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(twoDArray[i, size - 1 - i] + " ");
+        }
+    }
+}
